34db3058ac693997c1cd0165e519e681d4897973
bandhandut19 <bondon21081@gmail.com>
2023-01-04 18:31:02 +0600

    res check2

--- js/index.js
+++ js/index.js
@@ -3,29 +3,30 @@ let input = "";
 let buttons = document.querySelectorAll('.btns');
 
 Array.from(buttons).forEach((button) => {
+
     button.addEventListener('click', (pressed) => {
         console.log(pressed.target);
-        
-        if(pressed.target.innerHTML=='='){
-            try{
-                input=eval(input);
+        if (pressed.target.innerHTML == '=') {
+            try {
+
+                input = eval(input);
+                document.querySelector('input').value = input;
             }
             catch (e) {
                 if (e instanceof SyntaxError) {
                     alert(e.message);
-                    input= " ";
-                }            
-            document.querySelector('input').value=input;
-            
+                    input = " ";
+                    document.querySelector('input').value = input;
+                }
+            }
         }
-    }
-        else if(pressed.target.innerHTML=='C' ){
-            input= " ";
-            document.querySelector('input').value=input;
+        else if (pressed.target.innerHTML == 'C') {
+            input = " ";
+            document.querySelector('input').value = input;
         }
-        else{
+        else {
             input = input + pressed.target.innerHTML;
-            document.querySelector('input').value=input;
+            document.querySelector('input').value = input;
         }
     })
 })
