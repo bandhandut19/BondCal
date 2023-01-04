763a6431d8c958a03a372a3543f90773dfff9730
bandhandut19 <bondon21081@gmail.com>
2023-01-04 14:58:39 +0600

    res check1

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let input = "";
+
+let buttons = document.querySelectorAll('.btns');
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (pressed) => {
+        console.log(pressed.target);
+        
+        if(pressed.target.innerHTML=='='){
+            try{
+                input=eval(input);
+            }
+            catch (e) {
+                if (e instanceof SyntaxError) {
+                    alert(e.message);
+                    input= " ";
+                }            
+            document.querySelector('input').value=input;
+            
+        }
+    }
+        else if(pressed.target.innerHTML=='C' ){
+            input= " ";
+            document.querySelector('input').value=input;
+        }
+        else{
+            input = input + pressed.target.innerHTML;
+            document.querySelector('input').value=input;
+        }
+    })
+})
+
